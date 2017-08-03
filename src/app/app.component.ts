ffd83ee16e65985e8c32e1c37ff0b715a646af4e
Christian Valdez <cvaldezchavez@gmail.com>
2017-08-03 17:53:15 -0500

    commit init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,75 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  my_notes = [
+    {
+      id: 1,
+      title: 'Note 1',
+      description: 'This is description'
+    },
+    {
+      id: 2,
+      title: 'Note 1',
+      description: 'This is description'
+    },
+    {
+      id: 3,
+      title: 'Note 1',
+      description: 'This is description'
+    },
+    {
+      id: 4,
+      title: 'Note 1',
+      description: 'This is description'
+    }
+  ];
+
+  note = { id: null, title: null, description: null };
+
+  show_form = false;
+
+  editing = false;
+
+  addNote() {
+    this.show_form = true;
+    this.editing = false;
+  }
+
+  viewNote(note: any) {
+    this.show_form = true;
+    this.editing = true;
+    this.note = note;
+  }
+
+  cancel() {
+    this.show_form = false;
+  }
+
+  delete() {
+    const ME = this;
+    this.my_notes.forEach(function (element, index) {
+      if (element.id === ME.note.id) {
+        ME.my_notes.splice(index, 1);
+      }
+    });
+    this.show_form = false;
+    this.note = { id: null, title: null, description: null };
+  }
+
+  createNote() {
+    if (this.editing) {
+      const ME = this;
+      this.my_notes.forEach(function (element, index) {
+        if (element.id === ME.note.id) {
+          ME.my_notes[index] = ME.note;
+        }
+      });
+      this.show_form = false;
+    } else {
+      this.note.id = new Date();
+      this.my_notes.push(this.note);
+      this.show_form = false;
+      this.note = { id: null, title: null, description: null };
+    }
+  }
 }
